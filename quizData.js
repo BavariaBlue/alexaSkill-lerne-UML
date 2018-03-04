9fdfb75d8c3f43699e2d32a80217fcce44217d3a
BavariaBlue <JoRo@JoRoMBP15.fritz.box>
2018-03-04 15:27:43 +0100

    added source file quizData, quizMetaData and some other

---
+++ quizData.js
@@ -0,0 +1,109 @@
+// metadata for quizData.json
+
+// TODO: U M L instead of UML
+// TODO: sentence ending w/ or wo/ "."
+
+var test = {
+	"questions": [
+		// level 1 struktur -> struktur --> 7 questions (true)
+		{
+		},
+		// all: 7
+		// t/w: 7/0
+		
+		// level 1 verhalten - verhalten --> 7 questions (true)
+		{
+		},
+		// all: 14
+		// t/w: 14/0
+		
+		// level 1 struktur -> verhalten --> 7 questions (false)
+		{
+		},
+		// all: 21
+		// t/w: 14/7
+		
+		// level 1 verhalten - struktur --> 7 questions (false)
+		{
+		},
+		// all: 28
+		// t/w: 14/14
+	
+		// level 1 interaktion - interaktion --> 4 questions (true)
+		{
+		},
+		// all: 32
+		// t/w: 18/14
+		
+		// level 1 struktur - interaktion --> 7 questions (false)
+		{
+		},
+		// all: 39
+		// t/w: 18/21
+		
+		// level 1 verhalten - interaktion --> 3 questions (false)
+		{
+		},
+		// all: 42
+		// t/w: 18/24
+		
+		// Kategorisierung von Diagrammen in UML - grob
+        // level 1 categorize grob --> 2 questions (1 true, 1 false)
+        {
+        },
+		// all: 44        
+        // t/w: 19/25
+        
+        // level 1 categorize fein --> 6 questions (1 true, 5 false)
+        {
+        },
+		// all: 50        
+        // t/w: 20/30
+		
+		// level 1 general --> 7 questions (7 true)
+		{
+		},
+		// all: 57        
+        // t/w: 27/30
+		
+		
+		// level 2 general --> 1 question (1 true)
+		{
+		}
+		// all: 58
+		// t/w: 28/30
+		
+		// TODO level 2: questions for all diagram types
+
+		// (1) Profildiagramm
+		
+        // (2) Klassendiagramm
+
+        // (3) Kompositionsstrukturdiagramme
+
+        // (4) Komponentendiagramm
+
+        // (5) Verteilungsdiagramm
+
+        // (6) Objektdiagramm
+
+        // (7) Paketdiagramm
+
+        // (8) Aktivitätsdiagramm
+
+        // (9) Anwendungsfalldiagramm
+
+        // (10) Zustandsdiagramm
+
+        // (11) Sequenzdiagramm
+
+        // (12) Kommunikationsdiagramm
+
+        // (13) Interaktionsübersichtsdiagramm
+
+        // (14) Zeitverlaufsdiagramm
+
+		
+		// TODO TEST delete		
+	]	
+};
---
+++ src/index3.js
@@ -0,0 +1,55 @@
+'use strict';
+const Alexa = require("alexa-sdk");
+const appId = 'amzn1.ask.skill.b68a737d-c970-4e3f-bd2f-0a3327f28277';
+
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = appId;
+    // alexa.dynamoDBTableName = 'highLowGuessUsers';
+    alexa.registerHandlers(welcomeStudents);
+    alexa.execute();
+};
+const states = {	
+    
+    BEGINNERMODE: 'BEGINNER_MODE',                  // User opens the selection of Q/W for the absolut first time
+    PROFESSIONALMODE: 'PROFESSIONAL_MODE'           // User opens the selection of Q/W at least the second time
+   
+ };
+
+// route all incoming intent or launch requests to this handler.
+const newSessionHandler = {
+    // new session means starting the skill
+    'New Session': function(){
+        this.handler.state = states.BEGINNERMODE
+        // searchh in Attributes, if user opened skill for the first time:
+        if(Object.keys(this.attributes).length === 0){
+            // Great with long introduction.
+        }
+        else {
+            
+            // Great with shortcut introduction.
+        }
+    }
+}
+
+// ask for launching a new mode in beginners mode
+const launchModeBeginner = Alexa.CreateStateHandler(states.BEGINNERMODE) {
+    'LaunchMode'
+}
+
+
+
+// ich brauche kein state von GeneralStart zu Quiz oder Dictionary,
+// da die Intents für Starte Quiz, Score oder Dic in den STARTDIC... definiert sind.
+// aber ich brauche START und GUESS für jede Spielart, um zu modulieren und nicht Dialoge zu verwenden
+// im newSessionHandler lasse ich alles komplizierte und sage User er soll Quiz starten sagen. Score kann er selbt erfragen.
+/*
+    STARTQUIZMODE: 'START_QUIZ_MODE',               // user gave request to open quiz via "Öffne Quiz"
+    STARTDICTIONARYMODE: 'START_DICTIONARY_MODE',   // user gave request to open dictionary via "Öffne Wörterbuch"
+    STARTSCOREMODE: 'START_SCORE_MODE',             // user gave request to open Score via "Öffne Score"
+
+    
+    GUESSQUIZMODE: 'START_QUIZ_MODE',               // user is in quiz and has to answer with yes or no to question
+
+
+*/
