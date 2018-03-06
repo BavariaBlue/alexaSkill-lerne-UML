2d30c8896128f51d981d3fc28992750aabc0590b
BavariaBlue <johannesrottenkolber@icloud.com>
2018-03-06 10:08:47 +0100

    modified folder structure: added /src/data for all json and jsonMeta: added templates for dictionaryData & Meta.

---
+++ src/data/dictionaryMetaData.js
@@ -0,0 +1,28 @@
+// meta data for dictionaryData.json
+
+// first group: UML general
+
+    // UML √ √
+    // Diagrammtyp √ √
+
+// second group: UML sichten
+    // Sichten √ √ 
+    // Statische Sicht √ √
+    // Dynamische Sicht √ √ 
+
+
+// third group: UML diagrams types - general
+
+    // Strukturdiagramme √ √
+    // Verhaltensdiagramme √ √
+    // Interaktionsdiagramme √ √ 
+
+// fourth group: UML diagrams types - narrow
+
+    //
+
+// fifth group: UML diagram types elements
+
+    //
+
+// sixth group.....
